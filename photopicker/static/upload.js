9a97fd0b6602aa8a8cfab582c17a75bc37f773d2
Alex Morega <alex@grep.ro>
2014-03-17 09:44:12 +0200

    send file to server

--- photopicker/static/upload.js
+++ photopicker/static/upload.js
@@ -8,7 +8,26 @@ app.File = Backbone.Model.extend({
         this.domFile = domFile;
         this.set({
             'name': domFile.name,
-            'size': domFile.size
+            'size': domFile.size,
+            'state': 'queued'
+        });
+    },
+
+    upload: function(url) {
+        var formData = new FormData();
+        formData.append("file", this.domFile);
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                console.log('upload success');
+            },
+            error: function(jqXHR, textStatus, errorMessage) {
+                console.log('upload failed');
+            }
         });
     }
 
@@ -22,7 +41,8 @@ app.Uploader = Backbone.View.extend({
         'drop': 'on_drop'
     },
 
-    initialize: function() {
+    initialize: function(options) {
+        this.uploadUrl = options.uploadUrl;
         this.collection = new Backbone.Collection();
         this.statusView = new app.UploadStatus({collection: this.collection});
         this.$el.append(this.statusView.el);
@@ -36,8 +56,17 @@ app.Uploader = Backbone.View.extend({
         evt.preventDefault();
         var dataTransfer = evt.originalEvent.dataTransfer;
         _.forEach(dataTransfer.files, function(domFile) {
-            this.collection.add(new app.File(domFile));
+            var file = new app.File(domFile);
+            this.collection.add(file);
         }, this);
+
+        this.uploadNext();
+    },
+
+    uploadNext: function() {
+        var file = this.collection.findWhere({state: 'queued'});
+        if(! file) { return; }
+        file.upload(this.uploadUrl);
     }
 
 });
--- photopicker/upload.py
+++ photopicker/upload.py
@@ -7,3 +7,9 @@ upload = flask.Blueprint('upload', __name__)
 @upload.route('/upload')
 def upload_page():
     return flask.render_template('upload.html')
+
+
+@upload.route('/upload/save', methods=['POST'])
+def save():
+    print flask.request.files
+    return flask.jsonify(success=True)

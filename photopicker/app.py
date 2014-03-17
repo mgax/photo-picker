6fa834232061ac6db603e908ccd95862d26da3bf
Alex Morega <alex@grep.ro>
2014-03-17 21:18:20 +0200

    instance-relative config

--- photopicker/app.py
+++ photopicker/app.py
@@ -12,7 +12,8 @@ def ping():
 
 
 def create_app():
-    app = flask.Flask(__name__)
+    app = flask.Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile('settings.py', silent=True)
     app.register_blueprint(common)
     app.register_blueprint(upload.upload)
     return app

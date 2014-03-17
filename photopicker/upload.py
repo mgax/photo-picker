8ec1380bde55139310ba217e3d82e341c882c140
Alex Morega <alex@grep.ro>
2014-03-17 21:44:44 +0200

    save uploaded files

--- photopicker/upload.py
+++ photopicker/upload.py
@@ -1,7 +1,19 @@
+import tempfile
+import errno
+import hashlib
+from pathlib import Path
 import flask
+from werkzeug.wsgi import FileWrapper
 
 
 upload = flask.Blueprint('upload', __name__)
+
+
+@upload.record
+def create_storage(state):
+    app = state.app
+    container = Path(app.config['STORAGE_PATH'])
+    app.extensions['storage'] = FileStorage(container)
 
 
 @upload.route('/upload')
@@ -11,5 +23,64 @@ def upload_page():
 
 @upload.route('/upload/save', methods=['POST'])
 def save():
-    print flask.request.files
+    file = flask.request.files['file']
+    storage = flask.current_app.extensions['storage']
+    key = storage.create(file)
+    print 'saved file at', key
     return flask.jsonify(success=True)
+
+
+def _ensure(p, parents=True):
+    try:
+        p.mkdir(parents=parents)
+
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+
+class FileStorage(object):
+
+    create_hash = hashlib.sha1
+
+    def __init__(self, container):
+        self.container = Path(container)
+
+    @property
+    def tmp(self):
+        return self.container / 'tmp'
+
+    def get_path(self, key):
+        return self.container / key[:2] / key[2:]
+
+    def create(self, f):
+        _ensure(self.container, parents=True)
+        _ensure(self.tmp, parents=True)
+        hash = self.create_hash()
+        temp_file = tempfile.NamedTemporaryFile(
+            dir=str(self.tmp),
+            delete=False,
+        )
+        temp_file_path = Path(temp_file.name)
+
+        try:
+            with temp_file as tf:
+                for chunk in FileWrapper(f):
+                    hash.update(chunk)
+                    tf.write(chunk)
+
+            key = hash.hexdigest()
+            path = self.get_path(key)
+
+            _ensure(path.parent)
+
+        except:
+            temp_file_path.unlink()
+
+        else:
+            temp_file_path.rename(path)
+
+        return key
+
+    def open(self, key):
+        return self.get_path(key).open('rb')

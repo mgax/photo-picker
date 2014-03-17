159ccf8af8c161fa051f57644370bd7fe96b3fc2
Alex Morega <alex@grep.ro>
2014-03-17 21:58:07 +0200

    database model

--- photopicker/app.py
+++ photopicker/app.py
@@ -1,6 +1,7 @@
 import flask
 from flask.ext.script import Manager
 from photopicker import upload
+from photopicker import models
 
 
 common = flask.Blueprint('common', __name__)
@@ -14,6 +15,7 @@ def ping():
 def create_app():
     app = flask.Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('settings.py', silent=True)
+    models.db.init_app(app)
     app.register_blueprint(common)
     app.register_blueprint(upload.upload)
     return app
@@ -21,4 +23,5 @@ def create_app():
 
 def create_manager(app):
     manager = Manager(app)
+    manager.add_command('db', models.db_manager)
     return manager
---
+++ photopicker/models.py
@@ -0,0 +1,32 @@
+import uuid
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+
+
+db = SQLAlchemy()
+
+
+def generate_uuid():
+    return str(uuid.uuid4())
+
+
+class Album(db.Model):
+
+    id = db.Column(db.String, primary_key=True, default=generate_uuid)
+
+
+class Photo(db.Model):
+
+    id = db.Column(db.String, primary_key=True, default=generate_uuid)
+    album_id = db.Column(db.ForeignKey(Album.id))
+    storage_key = db.Column(db.String)
+
+    album = db.relationship(Album, backref='photos')
+
+
+db_manager = Manager()
+
+
+@db_manager.command
+def sync():
+    db.create_all()
